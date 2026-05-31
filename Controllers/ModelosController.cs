220084eabb20ff3133dd20b349ecf7ce5b08ed99
Erick Sena <erick087sena@gmail.com>
2026-05-31 20:38:11 -0300

    feat: adicionar endpoint GET de modelos

---
+++ Controllers/ModelosController.cs
@@ -0,0 +1,27 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OficinaJD.API.Data;
+using OficinaJD.API.Models;
+
+namespace OficinaJD.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModelosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ModelosController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Modelo>>> ObterModelos()
+        {
+            return await _context.Modelos.ToListAsync();
+        }
+    }
+}
