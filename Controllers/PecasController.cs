49d822d0f4f228cbfcfad865e3841679d55e32a7
Erick Sena <erick087sena@gmail.com>
2026-05-31 20:18:59 -0300

    feat: CRUD de peças concluído

--- Controllers/PecasController.cs
+++ Controllers/PecasController.cs
@@ -34,5 +34,44 @@ namespace OficinaJD.API.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(ObterPecas), new { id = peca.Id }, peca);
         }
+
+        [HttpGet("{codigo}")]
+        public async Task<ActionResult<Peca>> ObterPecaPorCodigo(string codigo)
+        {
+            var peca = await _context.Pecas
+                             .Include(p => p.PecaModelos)
+                             .ThenInclude(pm => pm.Modelo)
+                             .FirstOrDefaultAsync(p => p.Codigo == codigo);
+                if(peca == null)
+                  return NotFound();
+
+          return Ok(peca);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarPeca (int id, Peca peca)
+        {
+            if( id != peca.Id)
+             return BadRequest();
+
+             _context.Entry(peca).State = EntityState.Modified;
+             await _context.SaveChangesAsync();
+
+             return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarPeca (int id)
+        {
+            var peca = await _context.Pecas.FindAsync(id);
+
+            if(peca == null)
+               return NotFound();
+
+               _context.Pecas.Remove(peca);
+               await _context.SaveChangesAsync();
+
+               return NoContent();
+        }
     }
 }
